5c506154f5cb98f2defa1f20ee4861df1eccce5b
VincentBlase <vincent.blase20@gmail.com>
2020-08-11 16:47:54 +0200

    Update 11/08 #2

--- scripts/subset_3_dataCreate.R
+++ scripts/subset_3_dataCreate.R
@@ -6,14 +6,14 @@ if(subset_3){
       updateSelectInput(session,"subset_3",label = label3_select_1, choices = c("whole",levels(dat[,subset_3_names])),selected = subset_3_value[2])
     })
   }
-  dat_cut_subset_3<- dat
+  dat_cut_subset_3<- dat_cut_subset_2
   if ("whole"%in%subset_3_value){
-    dat_cut_subset_3 <- dat
+    dat_cut_subset_3 <- dat_cut_subset_2
   } else {
-    dat_cut_subset_3 <- dat[which(dat[,subset_3_names] == subset_3_value[1]),]
+    dat_cut_subset_3 <- dat_cut_subset_2[which(dat_cut_subset_2[,subset_3_names] == subset_3_value[1]),]
     if (length(subset_3_value) > 1){
       for (i in 2:length(subset_3_value)){
-        dat_cut_subset_3 <- rbind(dat_cut_subset_3,dat[which(dat[,subset_3_names] == subset_3_value[i]),])
+        dat_cut_subset_3 <- rbind(dat_cut_subset_3,dat_cut_subset_2[which(dat_cut_subset_2[,subset_3_names] == subset_3_value[i]),])
       }
     }
     dat_cut_subset_3[,subset_3_names] <- factor(dat_cut_subset_3[,subset_3_names],exclude=NULL)
@@ -39,14 +39,14 @@ if(subset_3){
   subset_3_1_value <- input$subset_3_1
   
   
-  dat_cut_subset_3 <- dat
+  dat_cut_subset_3 <- dat_cut_subset_2
   if ("whole"%in%subset_3_2_value){
-    dat_cut_subset_3 <- dat
+    dat_cut_subset_3 <- dat_cut_subset_2
   } else {
-    dat_cut_subset_3 <- dat[which(dat[,paste("subset_3.",subset_3_1_value,sep="")] == subset_3_2_value[1]),]
+    dat_cut_subset_3 <- dat_cut_subset_2[which(dat_cut_subset_2[,paste("subset_3.",subset_3_1_value,sep="")] == subset_3_2_value[1]),]
     if (length(subset_3_2_value)>1){
       for (i in 2:length(subset_3_2_value)){
-        dat_cut_subset_3 <- rbind(dat_cut_subset_3,dat[which(dat[,paste("subset_3.",subset_3_1_value,sep="")] == subset_3_2_value[i]),])
+        dat_cut_subset_3 <- rbind(dat_cut_subset_3,dat_cut_subset_2[which(dat[,paste("subset_3.",subset_3_1_value,sep="")] == subset_3_2_value[i]),])
       }
     }
     dat_cut_subset_3[,x_axis_names] <- factor(dat_cut_subset_3[,x_axis_names],exclude=NULL)
