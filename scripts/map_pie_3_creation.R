e4da77c338145cb9d1caf068117cc324dca339ed
VincentBlase <vincent.blase20@gmail.com>
2020-08-21 15:50:54 +0200

    Update 21/08 #2

--- scripts/map_pie_3_creation.R
+++ scripts/map_pie_3_creation.R
@@ -7,8 +7,8 @@ for(i in name_map_df_pie_3){
   rownames(Freq) <- d$pie_3
   Freq <- t(Freq)
   if (pie_1_valid | pie_2_valid | pie_4_valid){
-    prot_geo <- addMinicharts(map = prot_geo, lng = d$lng[1],lat = d$lat[1]-0.001, type = "pie", chartdata = Freq,colorPalette = pal_3,width = 75,showLabels = TRUE,opacity = 0.7,labelMinSize = 1,labelMaxSize = 32,layerId =  paste(substring(i,14),map_pie_3_value,sep = " - "),popup = popupArgs(labels = levels(map_df_pie_3)),legend = FALSE)
+    prot_geo <- addMinicharts(map = prot_geo, lng = d$lng[1],lat = d$lat[1]-0.001, type = "pie", chartdata = Freq,colorPalette = pal_3,width = map_pie_3_width,showLabels = TRUE,opacity = 0.7,labelMinSize = 1,labelMaxSize = 32,layerId =  paste(substring(i,14),map_pie_3_value,sep = " - "),popup = popupArgs(labels = levels(map_df_pie_3)),legend = FALSE)
   } else {
-    prot_geo <- addMinicharts(map = prot_geo, lng = d$lng[1],lat = d$lat[1], type = "pie", chartdata = Freq,colorPalette = pal_3,width = 75,showLabels = TRUE,opacity = 0.7,labelMinSize = 1,labelMaxSize = 32,layerId =  paste(substring(i,14),map_pie_3_value,sep = " - "),popup = popupArgs(labels = levels(map_df_pie_3)),legend = FALSE)
+    prot_geo <- addMinicharts(map = prot_geo, lng = d$lng[1],lat = d$lat[1], type = "pie", chartdata = Freq,colorPalette = pal_3,width = map_pie_3_width,showLabels = TRUE,opacity = 0.7,labelMinSize = 1,labelMaxSize = 32,layerId =  paste(substring(i,14),map_pie_3_value,sep = " - "),popup = popupArgs(labels = levels(map_df_pie_3)),legend = FALSE)
   }
 }
