691acac1841ff27f640c4abacd0cf9c13440650f
VincentBlase <vincent.blase20@gmail.com>
2020-08-25 16:23:41 +0200

    update 25/08 #3

--- scripts/update_UI_map_geo.R
+++ scripts/update_UI_map_geo.R
@@ -1,5 +1,4 @@
 if(geo_1_valid){
-  removeUI(".shiny-input-container:has(#map_geo_2)",immediate = TRUE)
   updateSelectInput(session,"map_geo_1", choices = c(levels(dat[,geo_1_names])))
 } else {
   removeUI(".shiny-input-container:has(#map_geo_1)",immediate = TRUE)
