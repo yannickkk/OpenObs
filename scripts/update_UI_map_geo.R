d67c0e562e8fceef8acdb58ab8dc512447aaf062
yannickkk <39738426+yannickkk@users.noreply.github.com>
2020-10-26 16:48:23 +0100

    Delete update_UI_map_geo.R

--- scripts/update_UI_map_geo.R
+++
@@ -1,7 +0,0 @@
-if(geo_1_valid){
-  updateSelectInput(session,"map_geo_1", choices = c(levels(dat[,geo_1_names])))
-} else {
-  removeUI(".shiny-input-container:has(#map_geo_1)",immediate = TRUE)
-}
-
-dat<-datt
